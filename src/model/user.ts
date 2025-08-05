2afa39bc275c015c5ace8de461b503b1608bfb2e
Ariel-250225 <seobin250225@icloud.com>
2025-08-05 14:32:38 +0700

    feature: 유저 핸들링 기능 추가

---
+++ src/model/user.ts
@@ -0,0 +1,23 @@
+export interface UserType {
+    id: number;
+    username: string;
+    level: number;
+    insertName: string;
+    insertDateTime: string;
+    updateName: string;
+    updateDateTime: string;
+    deleteName: string;
+    deleteDateTime: string;
+}
+
+export const levelLabelMap: Record<number, string> = {
+    3: "게스트",
+    2: "유저",
+    1: "VIP",
+    0: "관리자",
+};
+
+export interface SignInType {
+    name: string;
+    password: string;
+}
