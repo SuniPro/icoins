93b8a8a8e775e13d941fdd8d7d12cc69793104f4
SuniPro <SuniPro@users.noreply.github.com>
2025-08-05 07:41:40 +0000

    Prettified Code!

--- src/page/Transaction.tsx
+++ src/page/Transaction.tsx
@@ -19,34 +19,34 @@ const fadeUp = keyframes`
 `;
 
 export function Transaction() {
-    const theme = useTheme();
-    const { windowWidth } = useWindowContext();
+  const theme = useTheme();
+  const { windowWidth } = useWindowContext();
 
-    const { size } = useProportionHook(windowWidth, 600, theme.windowSize.tablet);
-    return (
-        <MainContainer width={size}>
-            <LogoContainer width={300} height={140}>
-                <Logo fontSize={80} />
-                <SubjectTitle theme={theme}>
-                    오직 당신을 위한 프라이빗 안전거래 시스템
-                </SubjectTitle>
-            </LogoContainer>
-            <StyledContentsContainer>
-                <InteractiveForm windowWidth={windowWidth} />
-            </StyledContentsContainer>
-        </MainContainer>
-    );
+  const { size } = useProportionHook(windowWidth, 600, theme.windowSize.tablet);
+  return (
+    <MainContainer width={size}>
+      <LogoContainer width={300} height={140}>
+        <Logo fontSize={80} />
+        <SubjectTitle theme={theme}>
+          오직 당신을 위한 프라이빗 안전거래 시스템
+        </SubjectTitle>
+      </LogoContainer>
+      <StyledContentsContainer>
+        <InteractiveForm windowWidth={windowWidth} />
+      </StyledContentsContainer>
+    </MainContainer>
+  );
 }
 
 const SubjectTitle = styled.span<{ theme: Theme; fontSize?: number }>(
-    ({ theme, fontSize }) => css`
+  ({ theme, fontSize }) => css`
     font-family: ${theme.mode.font.component.itemTitle};
     font-size: ${fontSize}px;
   `,
 );
 
 const MainContainer = styled.main<{ width: number }>(
-    ({ width }) => css`
+  ({ width }) => css`
     width: ${width}px;
     transform: translateY(1%);
     height: auto;
