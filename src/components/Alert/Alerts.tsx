c0247ef1030e3f96b3eeb8d2d34571117b6802cc
SuniPro <thtnsgus21@gmail.com>
2025-08-30 02:24:49 +0700

    refactor : confirm 알림 디자인 변경

--- src/components/Alert/Alerts.tsx
+++ src/components/Alert/Alerts.tsx
@@ -1,8 +1,9 @@
 import toast from "react-hot-toast";
 import styled from "@emotion/styled";
-import Button from "@mui/material/Button";
-import { useState } from "react";
-import { css, Theme, useTheme } from "@emotion/react";
+import { FuncItem } from "../styled/Button/Button";
+import { css, Theme } from "@emotion/react";
+import { ReactNode } from "react";
+import { Card } from "../layouts/Layouts";
 
 export function Alert(message: string) {
   toast(message, {
@@ -18,120 +19,52 @@ export function SuccessAlert(message: string): void {
   toast.success(message);
 }
 
-export function ConfirmAlert(message: string, func: () => void): void {
-  toast((t) => (
-    <ConfirmAlertContainer>
-      <span>{message}</span>
-      <ConfirmFuncBox>
-        <Button
-          variant="outlined"
-          color="error"
-          size="small"
-          onClick={() => toast.dismiss(t.id)}
-        >
-          NO
-        </Button>
-        <Button
-          variant="contained"
-          color="primary"
-          size="small"
-          onClick={() => {
-            toast.dismiss(t.id);
-            func(); // ✅ YES 누르면 실행
-          }}
-        >
-          YES
-        </Button>
-      </ConfirmFuncBox>
-    </ConfirmAlertContainer>
-  ));
-}
-
-export function showConfirmReceiptAlert(
-  message: string,
+export function CustomConfirmAlert(
+  customNode: ReactNode,
   func: () => void,
+  theme: Theme,
 ): void {
-  toast(<ConfirmReceiptAlert message={message} onConfirm={func} />);
-}
-
-function ConfirmReceiptAlert({
-  message,
-  onConfirm,
-}: {
-  message: string;
-  onConfirm: () => void;
-}) {
-  const [hashValue, setHashValue] = useState<string>("");
-  const theme = useTheme();
-
-  const isSha256Hash = (str: string): boolean => /^[a-f0-9]{64}$/i.test(str);
-
-  return (
-    <ConfirmAlertContainer>
-      <span>{message}</span>
-      <Description>아래에 거래 해시값을 입력해주세요.</Description>
-      <StyledInput
-        onChange={(e) => setHashValue(e.target.value)}
-        placeholder="txn hash를 입력해주세요."
-        isCurrent={isSha256Hash(hashValue)}
+  toast.custom(
+    (t) => (
+      <Card
+        className={`${t.visible ? "animate-custom-enter" : "animate-custom-leave"} max-w-md w-full bg-white pointer-events-auto`}
         theme={theme}
-      />
-      <ConfirmFuncBox>
-        <Button
-          variant="outlined"
-          color="error"
-          size="small"
-          onClick={() => toast.dismiss()}
-        >
-          NO
-        </Button>
-        <Button
-          variant="contained"
-          color="primary"
-          size="small"
-          onClick={() => {
-            if (!isSha256Hash(hashValue)) {
-              ErrorAlert("정확한 거래 해시값을 입력하세요.");
-            } else {
-              toast.dismiss();
-              onConfirm();
-            }
-          }}
-        >
-          YES
-        </Button>
-      </ConfirmFuncBox>
-    </ConfirmAlertContainer>
+      >
+        <div className="w-full flex flex-col gap-4">
+          {customNode}
+          <div className="w-full flex flex-row justify-between">
+            <StyledButton
+              func={() => toast.dismiss(t.id)}
+              label="취소"
+              backgroundColor={theme.colors.crimsonRed}
+              theme={theme}
+            />
+            <StyledButton
+              func={() => {
+                func();
+                toast.dismiss(t.id);
+              }}
+              label="확인"
+              backgroundColor={theme.mode.buttonActive}
+            />
+          </div>
+        </div>
+      </Card>
+    ),
+    {
+      duration: Infinity, // 자동으로 사라지지 않도록 설정
+    },
   );
 }
 
-const ConfirmAlertContainer = styled.div`
-  width: 350px;
-  gap: 20px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`;
+const StyledButton = styled(FuncItem)<{ backgroundColor: string }>(
+  ({ backgroundColor }) => css`
+    background-color: ${backgroundColor};
+    width: 45%;
+    color: white;
 
-const ConfirmFuncBox = styled.div`
-  width: 50%;
-  justify-content: space-between;
-  align-items: center;
-  display: flex;
-  flex-direction: row;
-`;
-
-const Description = styled.span`
-  margin-top: 5px;
-  font-size: 12px;
-`;
-
-const StyledInput = styled.input<{ theme: Theme; isCurrent: boolean }>(
-  ({ theme, isCurrent }) => css`
-    border: 1px solid ${isCurrent ? theme.mode.textAccent : theme.mode.warning};
-    width: 240px;
-    border-radius: ${theme.borderRadius.softBox};
-    height: 20px;
+    &:hover {
+      background-color: ${backgroundColor};
+    }
   `,
 );
