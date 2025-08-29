c167301080e15d20f589870b53dc05a1f3554d50
SuniPro <thtnsgus21@gmail.com>
2025-08-30 02:32:32 +0700

    feature : 입금 요청 페이지

---
+++ src/procedure/DepositRequest.tsx
@@ -0,0 +1,121 @@
+import { useTheme } from "@emotion/react";
+import { Card, CardContainer, HeadLine } from "@/components/layouts/Layouts";
+import { Input } from "@heroui/react";
+import { Step } from "./Step";
+import { amountRound, IndexStateProps } from "@/page/Main";
+import { useExchangeContext } from "@/context/ExchangeContext";
+import { CustomConfirmAlert, ErrorAlert } from "@/components/Alert";
+import { createCryptoDeposit } from "@/api/financial";
+import { useUserContext } from "@/context/UserContext";
+import { useEffect, useState } from "react";
+import { SiteResponseType } from "@/model/site";
+import { getSite } from "@/api/site";
+
+export function DepositRequest(props: { indexState: IndexStateProps }) {
+  const { user } = useUserContext();
+  const { setState } = props.indexState;
+  const { selectedCrypto, depositKrw, calculatedCryptoAmount } =
+    useExchangeContext();
+
+  const [siteObject, setSiteObject] = useState<SiteResponseType | null>(null);
+
+  useEffect(() => {
+    if (user) {
+      getSite(user.site).then((site) => {
+        setSiteObject(site);
+      });
+    }
+  }, [user]);
+
+  const theme = useTheme();
+
+  const depositConfirm = () => {
+    CustomConfirmAlert(
+      <>
+        <HeadLine theme={theme} textAlign="center">
+          정말 이대로 신청하시겠습니까?
+        </HeadLine>
+        <Input
+          endContent={
+            <div className="pointer-events-none flex items-center">
+              <span className="text-default-400 text-small">
+                {selectedCrypto}
+              </span>
+            </div>
+          }
+          readOnly
+          label={`입금해야할 ${selectedCrypto}`}
+          labelPlacement="outside"
+          type="number"
+          value={amountRound(selectedCrypto, calculatedCryptoAmount).toString()}
+        />
+      </>,
+      () =>
+        // console.log({
+        //   cryptoWallet: user!.cryptoWallet,
+        //   cryptoType: selectedCrypto,
+        //   amount: calculatedCryptoAmount.toString(),
+        //   krwAmount: depositKrw.toString(),
+        // }),
+        {
+          user &&
+            siteObject &&
+            createCryptoDeposit({
+              toAddress: siteObject.cryptoWallet,
+              fromAddress: user.cryptoWallet,
+              cryptoWallet: user.cryptoWallet,
+              cryptoType: selectedCrypto,
+              amount: amountRound(selectedCrypto, calculatedCryptoAmount),
+              krwAmount: depositKrw.toString(),
+            })
+              .then(() => setState(3))
+              .catch((e) => ErrorAlert(e.message));
+        },
+      theme,
+    );
+  };
+
+  const krwFormatter = new Intl.NumberFormat("ko-KR");
+
+  return (
+    <>
+      <CardContainer>
+        <Card theme={theme}>
+          <HeadLine theme={theme}>입금 신청</HeadLine>
+          <div className="flex flex-col gap-4 my-5">
+            <Input
+              endContent={
+                <div className="pointer-events-none flex items-center">
+                  <span className="text-default-400 text-small">
+                    {selectedCrypto}
+                  </span>
+                </div>
+              }
+              readOnly
+              label={`입금해야할 ${selectedCrypto}`}
+              labelPlacement="outside"
+              type="number"
+              value={amountRound(selectedCrypto, calculatedCryptoAmount)}
+            />
+            <Input
+              endContent={
+                <div className="pointer-events-none flex items-center">
+                  <span className="text-default-400 text-small">₩</span>
+                </div>
+              }
+              readOnly
+              label="신청금액 (원화)"
+              labelPlacement="outside"
+              type="text"
+              value={krwFormatter.format(depositKrw.toNumber())}
+            />
+            <HeadLine theme={theme} textAlign="center">
+              이 시세대로 신청을 원하신다면 다음을 누르세요.
+            </HeadLine>
+          </div>
+          <Step next={depositConfirm} previous={() => setState(1)} />
+        </Card>
+      </CardContainer>
+    </>
+  );
+}
