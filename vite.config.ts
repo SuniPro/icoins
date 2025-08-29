0404e222d7b40703a798f47a54958596822a1362
SuniPro <thtnsgus21@gmail.com>
2025-08-30 02:36:31 +0700

    feature : tailwind 설정 및 프록시 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,15 +11,32 @@ export default defineConfig({
         plugins: ["@emotion/babel-plugin"],
       },
     }),
+    tailwindcss(),
+    tsconfigPaths(),
   ],
   server: {
     host: true,
     port: 5020,
     proxy: {
-      "/user": {
+      "/forward": {
         target: "http://localhost:8020",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/user/, ""),
+        rewrite: (path) => path.replace(/^\/forward/, ""),
+        secure: false,
+        ws: true,
+      },
+      "/tracker": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/tracker/, ""),
+        secure: false,
+        ws: true,
+      },
+      "/admin": {
+        // target: "http://47.129.9.149:8010",
+        target: "http://localhost:8010",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/admin/, ""),
         secure: false,
         ws: true,
       },
