5973678dc27c9824c7180e7a3e51fd63c3363444
SuniPro <thtnsgus21@gmail.com>
2025-09-03 17:40:37 +0700

    style : lint 수정

--- src/provider.tsx
+++ src/provider.tsx
@@ -1,15 +1,9 @@
-import type { NavigateOptions } from "react-router-dom";
 import { useHref, useNavigate } from "react-router-dom";
 
 import { HeroUIProvider } from "@heroui/system";
+import { ReactNode } from "react";
 
-declare module "@react-types/shared" {
-  interface RouterConfig {
-    routerOptions: NavigateOptions;
-  }
-}
-
-export function Provider({ children }: { children: React.ReactNode }) {
+export function Provider({ children }: { children: ReactNode }) {
   const navigate = useNavigate();
 
   return (
