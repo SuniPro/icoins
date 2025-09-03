877ae1e8aa1ff5c7951716abf05f379fe18c4f9f
SuniPro <thtnsgus21@gmail.com>
2025-09-03 17:35:53 +0700

    refactor : site 외래키 추가에 따라 변경

--- src/api/site.ts
+++ src/api/site.ts
@@ -1,13 +1,13 @@
-import { SiteResponseType } from "@/model/site";
+import { SiteType } from "@/model/site";
 import { getFromEmployeeServer } from "./base";
 
-export async function getAllSites(): Promise<SiteResponseType[]> {
+export async function getAllSites(): Promise<SiteType[]> {
   const response = await getFromEmployeeServer("/site/get/all");
 
   return response.data;
 }
 
-export async function getSite(site: string): Promise<SiteResponseType> {
+export async function getSite(site: string): Promise<SiteType> {
   const response = await getFromEmployeeServer(`/site/get/${site}`);
 
   return response.data;
